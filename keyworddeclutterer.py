e90b19e5f2e581dafd0099ac88ec99cf6eecd7fb
jamesboswell84 <64914407+jamesboswell84@users.noreply.github.com>
2023-02-12 08:51:46 +0000

    Update keyworddeclutterer.py

--- keyworddeclutterer.py
+++ keyworddeclutterer.py
@@ -11,7 +11,9 @@ if 'df9' not in st.session_state:
 	st.session_state.df9 = None
 if 'df10' not in st.session_state:
 	st.session_state.df10 = None
-	
+if "alwaysshow" not in st.session_state:
+        st.session_state.alwaysshow = False	
+
 st.write("""
 	# 🧹 Keyword Declutterer
 	#### Merge and declutter your competitor keyword lists, removing >90% of the brand, irrelevant and nonsense keywords.
@@ -27,8 +29,9 @@ st.write("""
 files_xlsx = st.file_uploader("", accept_multiple_files=True, type=['xlsx'])
 
 ### Read files and create single dataframe
-if len(files_xlsx) > 2:	
+if len(files_xlsx) > 2:		
 	if st.button('Start merge & declutter'):
+	        st.session_state.alwaysshow = True
 		df = pd.DataFrame()
 		with st.spinner("Merging files..."):
 			progbar = st.progress(0)
@@ -41,9 +44,10 @@ if len(files_xlsx) > 2:
 				data["Site"] = sitename * len(data)
 				df = df.append(data)
 			st.session_state.df = df
+	files_xlsx = 3
 try:
 	### show and allow download of cluttered unedited file
-	if "df" in st.session_state:
+	if st.session_state.alwaysshow:
 		try:
 			st.write("""
 				#### Merged keyword list (cluttered):
@@ -63,8 +67,8 @@ try:
 		except NameError:
 			pass
 
-	### filter down keyword list 
-	if "df" in st.session_state:	
+		### filter down keyword list 
+
 		df2 = df.groupby(['Keyword','Site']).size().reset_index(name='Count')
 		df2 = df2[df2.Count < 2]
 		df2 = df2.merge(df,how="inner",on=["Keyword","Site"])
@@ -76,8 +80,8 @@ try:
 		df4 = df4.drop(['Count'], axis=1)
 		st.session_state.df4 = df4
 
-	### show and allow download of decluttered edited file
-	if "df4" in st.session_state:
+		### show and allow download of decluttered edited file
+
 		try:
 			st.write("""
 				#### Decluttered keyword list:
@@ -96,16 +100,13 @@ try:
 		except NameError:
 			pass
 
-	### pivot the data for a very quick SEMRush data look at estimated clicks by site
-	if "df4" in st.session_state:
+		### pivot the data for a very quick SEMRush data look at estimated clicks by site
 		df5 = pd.pivot_table(df4, values="Traffic", index="Site", aggfunc=sum).sort_values(by=['Traffic'], ascending=False)
 		st.session_state.df5 = df5
 		df6 = pd.pivot_table(df4, values="Traffic Cost", index="Site", aggfunc=sum).sort_values(by=['Traffic Cost'], ascending=False)
 		st.session_state.df6 = df6
 
-
-	### pivot the data for a very quick SEMRush data look at estimated clicks by sub-folder
-	if "df4" in st.session_state:	
+		### pivot the data for a very quick SEMRush data look at estimated clicks by sub-folder
 		df7 = df4
 		df7 = df7["URL"].str.split('/', expand=True)
 		df7 = df7.iloc[: , 3:].reset_index()
@@ -120,7 +121,6 @@ try:
 		df10 = pd.pivot_table(df7, values="Traffic Cost", index="Subfolder/Page", aggfunc=sum).sort_values(by=['Traffic Cost'], ascending=False)
 		st.session_state.df10 = df10
 
-	if "df5" in st.session_state:	
 		nametab1 = "Sites by traffic"
 		nametab2 = "Sites by traffic value ($CPC * traffic)"
 		nametab3 = "Subfolder/page by traffic"
