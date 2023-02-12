f34a0cc2ee5227459ab454b768e3a7f18c161e91
jamesboswell84 <64914407+jamesboswell84@users.noreply.github.com>
2023-02-12 08:07:16 +0000

    Update keyworddeclutterer.py

--- keyworddeclutterer.py
+++ keyworddeclutterer.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 import streamlit as st
+import altair.vegalite.v1 as alt
 
 ### The following is used to store the dataframes between reruns
 if 'df' not in st.session_state:
@@ -14,7 +15,7 @@ if 'df10' not in st.session_state:
 	
 st.write("""
 # 🧹 Keyword Declutterer
-### Merge and declutter your competitor keyword lists, removing 99% of the brand, irrelevant and nonsense keywords.
+### Merge and declutter your competitor keyword lists, removing >90% of the brand, irrelevant and nonsense keywords.
 """)
 st.write("""
 ## Simple 3-step setup:
@@ -70,61 +71,80 @@ if len(files_xlsx) > 2:
 			df4 = df4.drop(['Count'], axis=1)
 			st.session_state.df4 = df4
 
-			try:
-				st.write("""
-		## Decluttered keyword list:
-		""")
-				st.dataframe(df4[:100]) 
-				### The following prints the output and saves it to csv file
-				def convert_df(df4):
-				# IMPORTANT: Cache the conversion to prevent computation on every rerun
-					return df4.to_csv().encode('utf-8')
-				csv = convert_df(df4)
-				st.download_button('Download decluttered file (edited)', csv, file_name="decluttered_file.csv",mime='text/csv')
-			except TypeError:
-				pass
-			except AttributeError:
-				pass	
+		try:
+			st.write("""
+	## Decluttered keyword list:
+	""")
+			st.dataframe(df4[:100]) 
+			### The following prints the output and saves it to csv file
+			def convert_df(df4):
+			# IMPORTANT: Cache the conversion to prevent computation on every rerun
+				return df4.to_csv().encode('utf-8')
+			csv = convert_df(df4)
+			st.download_button('Download decluttered file (edited)', csv, file_name="decluttered_file.csv",mime='text/csv')
+		except TypeError:
+			pass
+		except AttributeError:
+			pass	
 
-			### pivot the data for a very quick SEMRush data look at estimated clicks by site
-			df5 = pd.pivot_table(df4, values="Traffic", index="Site", aggfunc=sum).sort_values(by=['Traffic'], ascending=False)
-			st.session_state.df5 = df5
-			df6 = pd.pivot_table(df4, values="Traffic Cost", index="Site", aggfunc=sum).sort_values(by=['Traffic Cost'], ascending=False)
-			st.session_state.df6 = df6
+		### pivot the data for a very quick SEMRush data look at estimated clicks by site
+		df5 = pd.pivot_table(df4, values="Traffic", index="Site", aggfunc=sum).sort_values(by=['Traffic'], ascending=False)
+		st.session_state.df5 = df5
+		df6 = pd.pivot_table(df4, values="Traffic Cost", index="Site", aggfunc=sum).sort_values(by=['Traffic Cost'], ascending=False)
+		st.session_state.df6 = df6
 
-			### pivot the data for a very quick SEMRush data look at estimated clicks by sub-folder
-			df7 = df4
-			df7 = df7["URL"].str.split('/', expand=True)
-			df7 = df7.iloc[: , 3:].reset_index()
-			df7 = df7.set_index("index").stack().reset_index(level=1, drop=True).to_frame("Sub").reset_index()
-			df8 = df4.reset_index()
-			df7 = df7.merge(df8,how="inner",on=["index"])
-			df7["Sub"] = df7["Sub"].replace(r'^s*$', float('NaN'), regex = True)
-			df7.dropna(inplace = True)
-			df7["Subfolder/Page"] = df7["Sub"]
-			df9 = pd.pivot_table(df7, values="Traffic", index="Subfolder/Page", aggfunc=sum).sort_values(by=['Traffic'], ascending=False)
-			st.session_state.df9 = df9
-			df10 = pd.pivot_table(df7, values="Traffic Cost", index="Subfolder/Page", aggfunc=sum).sort_values(by=['Traffic Cost'], ascending=False)
-			st.session_state.df10 = df10
-
-			try:
+		### pivot the data for a very quick SEMRush data look at estimated clicks by sub-folder
+		df7 = df4
+		df7 = df7["URL"].str.split('/', expand=True)
+		df7 = df7.iloc[: , 3:].reset_index()
+		df7 = df7.set_index("index").stack().reset_index(level=1, drop=True).to_frame("Sub").reset_index()
+		df8 = df4.reset_index()
+		df7 = df7.merge(df8,how="inner",on=["index"])
+		df7["Sub"] = df7["Sub"].replace(r'^s*$', float('NaN'), regex = True)
+		df7.dropna(inplace = True)
+		df7["Subfolder/Page"] = df7["Sub"]
+		df9 = pd.pivot_table(df7, values="Traffic", index="Subfolder/Page", aggfunc=sum).sort_values(by=['Traffic'], ascending=False)
+		st.session_state.df9 = df9
+		df10 = pd.pivot_table(df7, values="Traffic Cost", index="Subfolder/Page", aggfunc=sum).sort_values(by=['Traffic Cost'], ascending=False)
+		st.session_state.df10 = df10
+		
+		nametab1 = "Sites by traffic"
+		nametab2 = "Sites by traffic value ($CPC * traffic)"
+		nametab3 = "Subfolder/page by traffic"
+		nametab4 = "Subfolder/page by traffic value ($CPC * traffic)"
+		nametab5 = "$CPC versus difficulty"
+		tab1, tab2, tab3, tab4, tab5 = st.tabs([nametab1, nametab2, nametab3, nametab4,nametab5])
+		
+		with tab1:
+			if 'df5' in st.session_state:
 				st.write("""
-		## Sites by traffic:
-		""")
-				st.dataframe(df5[:100])
+					## Sites by traffic:
+				""")
+				st.dataframe(df5[:100])		
+		with tab2:
+			if 'df6' in st.session_state:
 				st.write("""
-		## Sites by traffic value ($CPC * traffic):
-		""")
-				st.dataframe(df6[:100])
+					## Sites by traffic value ($CPC * traffic):
+				""")
+			st.dataframe(df6[:100])
+		with tab3:
+			if 'df9' in st.session_state:
+			st.write("""
+				## Subfolder/page by traffic:
+			""")
+			st.dataframe(df9[:100])
+		with tab4:
+			if 'df10' in st.session_state:
 				st.write("""
-		## Subfolder/page by traffic:
-		""")
-				st.dataframe(df9[:100])
+					## Subfolder/page by traffic value ($CPC * traffic):
+				""")
+			st.dataframe(df10[:100])
+		with tab5:
+			if 'df5' in st.session_state:
 				st.write("""
-		## Subfolder/page by traffic value ($CPC * traffic):
-		""")
-				st.dataframe(df10[:100])
-			except TypeError:
-				pass
-			except AttributeError:
-				pass
+					## Subfolder/page by traffic value ($CPC * traffic):
+				""")				
+		#except TypeError:
+		#	pass
+		#except AttributeError:
+			#pass
